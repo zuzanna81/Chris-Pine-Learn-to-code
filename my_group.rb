78fa9e49d7706dc36bdb07b592afd8115e35248d
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-10-11 22:11:40 +0200

    my_group committed

---
+++ my_group.rb
@@ -0,0 +1,7 @@
+my_group = Array.new
+person1 = {name: "Annika", gender: 'female', age: 40}
+person2 = {name: "Kalle Anka", gender: 'male', age: 38}
+person3 = {name: "Alexander", gender: 'male', age: 35}
+my_group << person1
+my_group << person2
+my_group << person3
