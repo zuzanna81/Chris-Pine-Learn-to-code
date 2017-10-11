acdde0844a0e672f8e348f35642344850838140d
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-10-11 21:55:35 +0200

    third ruby file committed

---
+++ fullname.rb
@@ -0,0 +1,7 @@
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'What\'s your middlename?'
+middlename = gets.chomp
+puts 'What\'s your lastname?'
+lastname = gets.chomp
+puts 'Great to meet you, ' + name + ' ' + middlename + ' ' + lastname + '!'
