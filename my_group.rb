0f1bf8fa711ab9d58a811d0b730fb2d06acadde8
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-10-12 20:06:05 +0200

    committed again

--- my_group.rb
+++ my_group.rb
@@ -1,7 +1,10 @@
 my_group = Array.new
-person1 = {name: "Annika", gender: 'female', age: 40}
-person2 = {name: "Kalle Anka", gender: 'male', age: 38}
-person3 = {name: "Alexander", gender: 'male', age: 35}
-my_group << person1
-my_group << person2
-my_group << person3
+person_1 = {name: 'Annika', gender: 'woman', age: 40}
+person_2 = {name: 'Leif', gender: 'man', age: 35}
+person_3 = {name: 'Alexander', gender: 'man', age: 30}
+my_group << person_1
+my_group << person_2
+my_group << person_3
+my_group.each do |person|
+  puts person[:name] + " is a " + person[:age].to_s + " year old " + person[:gender] + "."
+end
